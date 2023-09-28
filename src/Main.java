cc409239cb7ae0d21c558b15fc7cc3cfba8cfb43
maviva97@gmail.com <maviva97@gmail.com>
2023-09-28 10:51:56 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+      long	CurrentWorldPopulation = 7811080815L;
+      int	CarsProducedThisYear = 55235358;
+      char	FirstLetterInAlphabet = 'A';
+      boolean	JavaIsAmazing = true;
+      double	Pi = 3.14159;
+        System.out.println("Current World Population - "+CurrentWorldPopulation);
+        System.out.println("Cars Produced This Year - "+CarsProducedThisYear);
+        System.out.println("First Letter In Alphabet - "+FirstLetterInAlphabet);
+        System.out.println("Java Is Amazing - "+JavaIsAmazing);
+        System.out.println("Pi - "+Pi);
+    }
+}
